bc81e748a7a64ee5f14045c481f3946fdf3493d3
Sheyzie <oluwaseyiolukosi7@gmail.com>
2025-05-19 19:20:52 +0100

    Carried out basic arithmetic operation in basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# carry out basic arithmetic opearations
+number1, number2 = 10, 5
+
+sum1 = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {sum1}')
+print(f'Subtraction of {number1} and {number2} is {sub}')
+print(f'Multiplication of {number1} and {number2} is {mult}')
+
