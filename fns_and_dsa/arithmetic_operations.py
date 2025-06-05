554da80ef8cd397a4db136b02da48adbaa52d3d1
Sheyzie <oluwaseyiolukosi7@gmail.com>
2025-06-05 11:45:19 +0100

    added elif to for no reason

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,7 +13,9 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return 'Cannot divide by zero'
-            else: 
+            elif num2 > 0: 
+                return num1 / num2
+            else:
                 return num1 / num2
         case _:
             return 'unknown operation'
