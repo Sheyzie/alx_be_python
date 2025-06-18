05566760b3e17c802bfc2a933ab7df8e2c408e46
Sheyzie <oluwaseyiolukosi7@gmail.com>
2025-06-18 11:27:04 +0100

    Added book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class Book:
+
+  def __init__(self, title: str, author: str, year: int):
+    self.title = title
+    self.author = author
+    self.year = year
+
+  def __str__(self):
+    return f'{self.title} by {self.author} published in {self.year}'
+  
+  def __repr__(self):
+    return f'{self.__class__.__name__}({self.title}, {self.author}, {self.year})'
+  
+  def __del__(self):
+    print(f'Deleting {self.title}')
